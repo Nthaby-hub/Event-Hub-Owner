e7a610114b828753b400c4e713f9a1a26277b451
banelekhumalo <67458707+banelekhumalo@users.noreply.github.com>
2020-11-26 17:21:59 +0200

    New

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LoadingController, ModalController, NavController } from '@ionic/angular';
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+import { EventsService } from '../services/events.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPage implements OnInit {
 
-  constructor() { }
+  uid = this.activatedActivated.snapshot.params.id;
+  events: Array<any> = [];
+  id: any;
+
+  constructor(private router:Router,
+    public nav:NavController,
+    private loadingController: LoadingController,
+    private modalCtrlr: ModalController,
+    
+    private activatedActivated: ActivatedRoute, 
+    private eventService: EventsService) { }
 
   ngOnInit() {
+    // this.eventService.signAuth()
+    firebase.firestore().collection('events').onSnapshot(res => {
+      res.forEach(element => {
+        this.events.push(element.data());
+      });
+    });
+  
+  }
+  goToNext()
+  {
+    this.router.navigateByUrl('/create-event');
   }
 
+  
 }
---
+++ src/app/services/events.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor(private router: Router) { }
+//-------------------Banele
+  regEvent() {
+    return firebase.firestore().collection('events');
+  }
+
+  Signup_Owner(email,password) 
+  {
+    return firebase.auth().createUserWithEmailAndPassword(email, password); 
+  }
+  reserve() {
+    return firebase.firestore().collection('events');
+  }
+}
